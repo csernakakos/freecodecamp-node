66b9bdf9a530cbc7ff251ff0bf66ec6c4c81ad30
csernakakos <csernakakos@gmail.com>
2022-05-13 20:03:05 +0200

    Project 4: query parameters

--- 04-store-api/controllers/products.js
+++ 04-store-api/controllers/products.js
@@ -1,9 +1,37 @@
+const Product = require("../models/product");
+
+
 const GET_AllProductsStatic = async (req, res) => {
-    res.send("GET_ALLProductsStatic");
+    const products = await Product.find({ name: "vase table"});
+    
+    res.status(200).json({
+        status: "success",
+        count: products.length,
+        products,
+    });
 }
 
 const GET_AllProducts = async (req, res) => {
-    res.send("GET_AllProducts");
+    const { featured } = req.query;
+
+    /*
+    Create our own query object. To this object, only the query parameters we specify will be added. For example, "featured=..." will be added. "akos=..." will not be added.
+    */
+    const queryObject = {};
+
+    if (featured) {
+        queryObject.featured = featured === "true" ? true : false;
+
+    }
+
+    console.log("QUERYOBJECT: ", queryObject)
+    const products = await Product.find(queryObject);
+
+    res.status(200).json({
+        status: "success",
+        count: products.length,
+        products,
+    })
 }
 
 module.exports = {
