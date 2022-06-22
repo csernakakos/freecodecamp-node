3498b7dafd1545b47241ba20e3d7d24e9b3fb547
csernakakos <csernakakos@gmail.com>
2022-06-22 20:55:04 +0200

    LocalStorage

--- js-shopping-cart/main.js
+++ js-shopping-cart/main.js
@@ -31,12 +31,15 @@ const shopItemsData = [
     },
 ];
 
-let basket = [];
+let basket = JSON.parse(localStorage.getItem("data")) || [];
+console.log(basket);
 
 function generateShop() {
     return (shop.innerHTML = shopItemsData.map((item) => {
 
         const { id, name, price, desc, img } = item;
+
+        let search = basket.find((item) => item.id === id) || [];
 
         return `
             <div class="item" id="product-id-${id}">
@@ -49,7 +52,7 @@ function generateShop() {
                         <h2>$${price}</h2>
                         <div class="buttons">
                             <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
-                            <div class="quantity" id=${id}>0</div>
+                            <div class="quantity" id=${id}>${search.item === undefined ? 0 : search.item}</div>
                             <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
                         </div>
                     </div>
@@ -73,6 +76,7 @@ function increment(id) {
         search.item += 1;
     }
 
+    localStorage.setItem("data", JSON.stringify(basket));
     update(selectedItem.id);
 }
 
@@ -80,21 +84,34 @@ function decrement(id) {
     const selectedItem = id;
     let search = basket.find((item) => item.id === selectedItem.id);
 
+    if (!search) return;
+
     if (search.item === 0) return
     else {
         search.item -= 1;
     }
 
+    
+    basket = basket.filter((item) => item.item > 0);
     update(selectedItem.id);
+    localStorage.setItem("data", JSON.stringify(basket));
 }
 
 function update(id) {
     let search = basket.find((item) => item.id === id);
-
     document.querySelector(`#${id}`).innerHTML = search.item;
-    
-    
+    calculateTotal();
 }
 
+function calculateTotal() {
+    const totalDOM = document.querySelector(".cartAmount");
+
+    const sumOfItems = basket.reduce((sum, item) => {
+        return sum + item.item;
+    }, 0);
+
+    totalDOM.innerHTML = sumOfItems;
+}
 
 generateShop();
+calculateTotal();
